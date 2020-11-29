feb07c176fd934cd3dfa323d54996c71202c5d83
Alain <alainrodriguezz@gmail.com>
2020-11-28 20:10:39 -0500

    Agregado About Contact y ContactController

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
@@ -38,9 +39,8 @@
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                    <h1>Home</h1>
+                    <h1>About</h1>
                 </div>
-
                 <div>
                     <a href="/">Home</a> |
                     <a href="/about">About</a>  |
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -38,9 +38,8 @@
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                    <h1>Home</h1>
+                    <h1>Contact</h1>
                 </div>
-
                 <div>
                     <a href="/">Home</a> |
                     <a href="/about">About</a>  |
